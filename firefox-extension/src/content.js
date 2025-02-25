67245c0e3197555b166df062a02bd34f88238a1e
Rahul Vaidun <19789246+rvaidun@users.noreply.github.com>
2025-02-24 19:52:44 -0800

    add host name

--- firefox-extension/src/content.js
+++ firefox-extension/src/content.js
@@ -1,6 +1,5 @@
 console.log('Content script loadedasdfasdf');
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    console.log('Message received:', request);
     if (request.action === 'toggleMute') {
         if (request.mute) {
             muteAllAudio();
@@ -9,14 +8,21 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
         }
     }
 });
-chrome.storage.sync.get('muteEnabled', function (data) {
-    console.log('Mute enabled:', data.muteEnabled);
-    if (data.muteEnabled) {
+// get the hostname of current tab
+const url = window.location.href;
+const hostname = new URL(url).hostname;
+const key = `muteEnabled-${hostname}`;
+// chrome.storage.sync.get('muteEnabled', function (data) {
+//     if (data.muteEnabled) {
+//         muteAllAudio();
+//     }
+// });
+chrome.storage.sync.get(key, function (data) {
+    if (data[key]) {
         muteAllAudio();
     }
 });
 function muteAllAudio() {
-    console.log('Muting all audio');
     const audioElements = document.querySelectorAll('audio, video');
     audioElements.forEach(audio => {
         audio.muted = true;
@@ -24,7 +30,6 @@ function muteAllAudio() {
 }
 
 function unmuteAllAudio() {
-    console.log('Unmuting all audio');
     const audioElements = document.querySelectorAll('audio, video');
     audioElements.forEach(audio => {
         audio.muted = false;
--- firefox-extension/src/options.js
+++ firefox-extension/src/options.js
@@ -3,55 +3,54 @@
 //         chrome.tabs.sendMessage(tab.id, { message: "your message here" });
 //     });
 // });
-
-
-function getMutedState() {
-    chrome.storage.sync.get('muteEnabled', data => {
-        console.log('Mute enabled:', data.muteEnabled);
-        if (data.muteEnabled) {
+let currentHostName = '';
+function setMutedToggle() {
+    const key = `muteEnabled-${currentHostName}`;
+    chrome.storage.sync.get(key, data => {
+        console.log('Data:', data);
+        console.log('Key:', key);
+        console.log('Data[key]:', data[key]);
+        if (data[key]) {
             document.querySelector('#muteToggle').classList.add('checked');
         }
     });
 }
 
-function saveOptions(e) {
-    // e.preventDefault();
-    console.log('Saving options');
-    console.log(e)
-    const isChecked = document.querySelector('#muteToggle').classList.contains('checked');
-    console.log('Checked:', isChecked);
-    chrome.storage.sync.set({
-        muteEnabled: isChecked
-    });
-
-    chrome.tabs.query({}, tabs => {
-        console.log('Tabs:', tabs);
-        tabs.forEach(tab => {
-            chrome.tabs.sendMessage(tab.id, { action: 'toggleMute', mute: isChecked });
-        });
-    })
+function saveOptions(isChecked) {
+    const key = `muteEnabled-${currentHostName}`;
+    console.log('Saving:', isChecked, key);
+    chrome.storage.sync.set({ [key]: isChecked });
 }
-let clickedTimes = 0;
-document.addEventListener('DOMContentLoaded', () => {
+function setWebsiteNameInPopup() {
+    // get the name of the current tab
+    console.log('Setting website name');
+    chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+        const url = tabs[0].url;
+        // convert URL to hostname
+        const hostname = new URL(url).hostname;
+        currentHostName = hostname;
+        console.log('currentHostName:', currentHostName);
+        document.querySelector('#websiteNameInjector').innerHTML = currentHostName;
+        setMutedToggle();
+    });
+}
+
+function getHostName() {
+}
+document.addEventListener('DOMContentLoaded', async () => {
     // get the current state of the muteEnabled option and save it to a boolean
-    console.log('Document loaded');
-    getMutedState();
-    // when checkbox with id muteToggle is clicked, saveOptions is called
-    // document.querySelector('#muteToggle').addEventListener('change', saveOptions);
-    //    document.querySelector('.toggle-container').addEventListener('click', saveOptions);
-    // document.querySelector('#muteToggle2').addEventListener('click', () => {
-    //     // document.querySelector("#muteToggle").innerHTML = `Clicked ${++clickedTimes} times`;
-    //     document.querySelector('#muteToggle').classList.toggle('checked');
+    setWebsiteNameInPopup();
 
-
-    // });
     document.querySelector('#muteToggle').addEventListener('click', () => {
         // document.querySelector("#muteToggle").innerHTML = `Clicked ${++clickedTimes} times`;
         document.querySelector('#muteToggle').classList.toggle('checked');
-        saveOptions();
-
-
+        const isChecked = document.querySelector('#muteToggle').classList.contains('checked');
+        saveOptions(isChecked);
+        // send a message to active tab to mute/unmute audio
+        chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+            const tabId = tabs[0].id;
+            chrome.tabs.sendMessage(tabId, { action: 'toggleMute', mute: isChecked });
+        });
     });
     console.log('Event listener added');
-
 });
